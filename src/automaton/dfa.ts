07a163725d0b258822e011490e612e0cdbb3c41d
hsjoihs / sozysozbot <sozysozbot@gmail.com>
2024-10-31 07:14:36 +0900

    Add functions `runSingleStep` and `runAutomaton`

--- src/automaton/dfa.ts
+++ src/automaton/dfa.ts
@@ -7,3 +7,20 @@ export type DFA = {
     transition_table: ReadonlyMap<AutomatonState, ReadonlyMap<Alphabet, AutomatonState>>,
     accept_states: ReadonlyArray<AutomatonState>
 };
+
+export function runSingleStep(dfa: DFA, state: AutomatonState, char: Alphabet): AutomatonState {
+    return dfa.transition_table.get(state)!.get(char as Alphabet)!;
+}
+
+export function runAutomaton(dfa: DFA, input_str: string): boolean {
+    let state: AutomatonState = dfa.initial_state;
+    for (const char of input_str) {
+        if (dfa.alphabets.includes(char as Alphabet) === false) {
+            throw new Error(`Invalid character: the alphabet set is {"${
+                dfa.alphabets.join('", "')
+            }"} but encountered a character "${char}"`);
+        }
+        state = runSingleStep(dfa, state, char as Alphabet);
+    }
+    return dfa.accept_states.includes(state);
+}
